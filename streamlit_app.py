8ed0353c29bef46ce758ccc383a9f6dc8f0eb9c0
tannerng79mn <tannerng79mn@gmail.com>
2026-01-06 01:42:48 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,10 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 # Convert Snowpark dataframe -> Python list for Streamlit widget
 fruit_rows = my_dataframe.collect()
